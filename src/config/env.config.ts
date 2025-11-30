16b97380c2310a6087ca67de87a491911c42f452
Sajmul <sajmul1427@gmail.com>
2025-11-30 20:36:58 +0600

    env config file created at next level

---
+++ src/config/env.config.ts
@@ -0,0 +1,21 @@
+interface IEnv {
+  port: string;
+  db_uri: String;
+}
+
+const envs = ["PORT", "DB_URI"];
+
+const loadEnv = (): IEnv => {
+  envs.forEach((env) => {
+    if (!process.env[env]) {
+      throw new Error(env + " not found!!! at env config...");
+    }
+  });
+
+  return envs.reduce((acc, env) => ({
+      ...acc,
+      [env.toLowerCase()]: process.env[env],
+    }), {} as IEnv);
+};
+
+export default loadEnv();
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from "./app";
+import envConfig from "./config/env.config";
 
 app.listen(5000, () =>{
-    console.log(`Server is running at port: `);
+    console.log(`Server is running at port: ${envConfig.PORT}`);
+    console.log(envConfig);
 })
