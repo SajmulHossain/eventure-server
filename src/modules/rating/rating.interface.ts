822d20857954249463887a0b8ff3c05e5ee3905a
Sajmul <sajmul1427@gmail.com>
2025-12-01 20:15:06 +0600

    rating interface and model done

---
+++ src/modules/rating/rating.interface.ts
@@ -0,0 +1,7 @@
+export interface IRating {
+  userId: string;
+  hostId: string;
+  eventId: string;
+  rating: number;
+  comment: string;
+}
---
+++ src/modules/rating/rating.model.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+import type { IRating } from "./rating.interface";
+
+const ratingModel = new Schema<IRating>({
+  userId: {
+    type: String,
+    required: true,
+  },
+  hostId: {
+    type: String,
+    required: true,
+  },
+
+  eventId: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+});
+
+export const Rating = model<IRating>("Rating", ratingModel);
