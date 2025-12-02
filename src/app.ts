2f65d0b22a59ae15ac97cb3f312f291e9f934499
Sajmul <sajmul1427@gmail.com>
2025-12-02 18:08:24 +0600

    passport intialization done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
+import { globalErrorHandler } from "@middlewares/globalErrorHandler";
+import { notFoundHandler } from "@middlewares/not-found";
 import express from "express";
 import router from "./routes/routes.index";
-import { notFoundHandler } from "@middlewares/not-found";
+import expressSession from "express-session";
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  expressSession({
+    secret: "default_secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
 
 app.use("/api/v1", router);
 
@@ -12,6 +30,7 @@ app.get("/", (_req, res) => {
   });
 });
 
+app.use(globalErrorHandler);
 app.use(notFoundHandler);
 
 export default app;
