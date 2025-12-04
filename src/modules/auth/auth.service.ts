a09f837490e47f9eca36c9321c7a67fdc418a0f1
Sajmul <sajmul1427@gmail.com>
2025-12-04 17:57:00 +0600

    update password and update profile service done

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { IUser } from "@modules/user/user.interface";
 import { User } from "@modules/user/user.model";
 import { ApiError } from "@utils/ApiError";
-import { hashPassword } from "@utils/hashPassword";
+import { hashPassword, hashPassword } from "@utils/hashPassword";
+import { compare } from "bcryptjs";
 
 const register = async ({ email, password, ...rest }: Partial<IUser>) => {
   const isUserExist = await User.findOne({ email });
@@ -28,6 +29,37 @@ const register = async ({ email, password, ...rest }: Partial<IUser>) => {
   return user;
 };
 
+const updateProfile = async (userId: string, updateData: Partial<IUser>) => {
+  const user = await User.findByIdAndUpdate(userId, updateData, { new: true });
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  user.password = undefined;
+  return user;
+};
+
+const changePassword = async (userId: string, password:{oldPassword: string, newPassword: string }) => {
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isMatch = await compare(password.oldPassword, user.password as string);
+  if (!isMatch) {
+    throw new ApiError(400, "Old password is incorrect");
+  }
+
+  const hashedPassword = await hashPassword(password.newPassword);
+
+  user.password = hashedPassword;
+  await user.save();
+  user.password = undefined;
+  return user;
+}
+
 export const AuthServices = {
     register,
+    updateProfile,
+    changePassword,
 }
